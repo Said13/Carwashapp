876c11f4dc40f0e33f8000223a9a348fd453d25e
said13 <abdullah.atkaev@gmail.com>
2017-01-25 07:51:21 +0200

    Initial Commit

---
+++ Carwashapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Carwashapp
+//
+//  Created by Abdullah on 25.01.17.
+//  Copyright © 2017 Abdullah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
